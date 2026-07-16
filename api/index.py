9fac0bf183525ce1f83eb00968dfec2104f65b4a
Aadi Gupta <iamgupta@ucdavis.edu>
2026-07-16 00:27:28 -0700

    Link vendored OpenMP library for LightGBM

--- api/index.py
+++ api/index.py
@@ -10,6 +10,18 @@ for site_packages in [*site.getsitepackages(), *sys.path]:
     if not site_packages:
         continue
     for libgomp_path in glob.glob(os.path.join(site_packages, "*.libs", "libgomp*.so*")):
+        for link_dir in [
+            os.path.dirname(libgomp_path),
+            os.path.join(site_packages, "lightgbm", "lib"),
+        ]:
+            try:
+                os.makedirs(link_dir, exist_ok=True)
+                link_path = os.path.join(link_dir, "libgomp.so.1")
+                if not os.path.exists(link_path):
+                    os.symlink(libgomp_path, link_path)
+            except OSError:
+                pass
+
         ctypes.CDLL(libgomp_path, mode=ctypes.RTLD_GLOBAL)
         break
 
